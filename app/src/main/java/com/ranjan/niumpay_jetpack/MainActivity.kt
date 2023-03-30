41a421030ec7a32ee22343e2d4c54d90d98eefa3
ranjan_96 <prakash.ranjan@nium.com>
2023-03-30 18:31:53 +0530

    Home Page UI.

--- app/src/main/java/com/ranjan/niumpay_jetpack/MainActivity.kt
+++ app/src/main/java/com/ranjan/niumpay_jetpack/MainActivity.kt
@@ -4,27 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.AccountCircle
-import androidx.compose.material.icons.outlined.ExitToApp
-import androidx.compose.material.icons.outlined.Info
-import androidx.compose.material.icons.outlined.Notifications
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
 import com.ranjan.niumpay_jetpack.model.TransactionItem
 import com.ranjan.niumpay_jetpack.model.UserCard
 import com.ranjan.niumpay_jetpack.ui.theme.NIUMPayJetpackTheme
-import com.ranjan.niumpay_jetpack.ui.views.*
-import java.util.*
+import com.ranjan.niumpay_jetpack.ui.views.CardsUI
+import com.ranjan.niumpay_jetpack.ui.views.QuickActionsUI
+import com.ranjan.niumpay_jetpack.ui.views.ToolbarUI
+import com.ranjan.niumpay_jetpack.ui.views.TransactionsUI
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,7 +33,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     val list = ArrayList<UserCard>()
                     val transactionList = ArrayList<TransactionItem>()
-                    repeat(10) {
+                    repeat(20) {
                         list.add(UserCard(1234, "234234234323", 321, "", "ranjan", "", "", "", ""))
                         transactionList.add(
                             TransactionItem(
@@ -58,56 +54,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun HomeUI(
     cardList: ArrayList<UserCard>, transactionList: ArrayList<TransactionItem>
 ) {
-    var height by remember { mutableStateOf(230.dp) }
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        Column(modifier = Modifier.background(color = Color.Blue.copy(alpha = 0.8f))) {
-            Row(modifier = Modifier.padding(10.dp)) {
-                ActionIcons(Icons.Outlined.ExitToApp, contentDescription = "Exit")
-                Spacer(modifier = Modifier.weight(1f))
-                ActionIcons(Icons.Outlined.Notifications, contentDescription = "Notification")
-                ActionIcons(Icons.Outlined.Info, contentDescription = "Info")
-                ActionIcons(Icons.Outlined.AccountCircle, contentDescription = "Profile")
-            }
-
-            HorizontalPager(
-                count = cardList.size + 1,
-                modifier = Modifier.padding(top = 5.dp),
-                contentPadding = PaddingValues(start = 20.dp, end = 20.dp),
-                itemSpacing = 10.dp
-            ) {
-                if (it < cardList.size) Column(
-                    modifier = Modifier
-                        .padding(10.dp)
-                        .fillMaxWidth()
-                ) {
-                    height = cardItem(card = cardList[it])
-                    Button(
-                        onClick = { /*TODO*/ },
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
-                    ) {
-                        Text(text = "Add To Google Pay")
-                    }
-                }
-                else EmptyCardItem(height = height)
-            }
-        }
-        QuickActionsUI()
-        LazyColumn(
-            modifier = Modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.spacedBy(10.dp),
-            contentPadding = PaddingValues(10.dp)
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color.Blue.copy(alpha = 0.8f))
         ) {
-            items(items = transactionList) {
-                TransactionItemUI(transaction = it)
-                Divider()
-            }
+            ToolbarUI()
+            CardsUI(cardList)
+        }
+        Column(Modifier.padding(horizontal = 20.dp)) {
+            QuickActionsUI()
+            TransactionsUI(transactionList)
         }
     }
 }
--- app/src/main/java/com/ranjan/niumpay_jetpack/ui/views/HomeUI.kt
+++ app/src/main/java/com/ranjan/niumpay_jetpack/ui/views/HomeUI.kt
@@ -1,29 +1,28 @@
 package com.ranjan.niumpay_jetpack.ui.views
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.ExitToApp
-import androidx.compose.material.icons.rounded.AccountCircle
-import androidx.compose.material.icons.rounded.Send
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.ExitToApp
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -31,25 +30,72 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.ranjan.niumpay_jetpack.R
 import com.ranjan.niumpay_jetpack.model.TransactionItem
 import com.ranjan.niumpay_jetpack.model.UserCard
 
 @Composable
-@Preview(showBackground = true)
-fun CardItemPreview() {
-    val userCard = UserCard(1234, "23423", 321, "", "ranjan", "", "", "", "")
-    val transactionItem = TransactionItem(12, "USD", "Debit", "Remittance", "ranjan", "Now", "Done")
-    Column {
-//        cardItem(card = userCard)
-//        TransactionItemUI(transactionItem)
-//        QuickActionsUI()
-//        ActionIcons(Icons.Outlined.AccountBox, contentDescription = "asdcasd")
+fun ToolbarUI() {
+    Row(modifier = Modifier.padding(10.dp)) {
+        ActionIcons(Icons.Outlined.ExitToApp, contentDescription = "Exit")
+        Spacer(modifier = Modifier.weight(1f))
+        ActionIcons(Icons.Outlined.Notifications, contentDescription = "Notification")
+        ActionIcons(Icons.Outlined.Info, contentDescription = "Info")
+        ActionIcons(Icons.Outlined.AccountCircle, contentDescription = "Profile")
     }
 }
 
 @Composable
-fun cardItem(card: UserCard): Dp {
-    var imageHeight by remember { mutableStateOf(0.dp) }
+fun ActionIcons(
+    icon: ImageVector,
+    contentDescription: String = "",
+    padding: Dp = 5.dp,
+    onClickListener: (() -> Unit)? = null
+) {
+    val iconModifier = Modifier
+        .clip(CircleShape)
+        .clickable {
+            onClickListener?.invoke()
+        }
+    Icon(
+        imageVector = icon,
+        contentDescription = contentDescription,
+        modifier = iconModifier
+            .padding(padding)
+            .shadow(2.dp, CircleShape)
+    )
+
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun CardsUI(cardList: ArrayList<UserCard>) {
+    HorizontalPager(
+        count = cardList.size + 1,
+        modifier = Modifier,
+        contentPadding = PaddingValues(start = 20.dp, end = 20.dp),
+        itemSpacing = 10.dp,
+        verticalAlignment = Alignment.Top
+    ) {
+        if (it < cardList.size) Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            CardItem(card = cardList[it])
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+            ) {
+                Text(text = "Add To Google Pay")
+            }
+        }
+        else EmptyCardItem()
+    }
+}
+
+@Composable
+fun CardItem(card: UserCard) {
     Card(
         shape = RoundedCornerShape(14.dp),
         backgroundColor = MaterialTheme.colors.surface,
@@ -57,48 +103,41 @@ fun cardItem(card: UserCard): Dp {
             .fillMaxWidth()
             .clip(RoundedCornerShape(8.dp))
     ) {
-        val density = LocalDensity.current
-        Image(
-            painter = painterResource(id = card.cardImage),
-            contentDescription = "cardImage",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .fillMaxWidth()
-                .onSizeChanged {
-                    imageHeight = with(density) { it.height.toDp() }
-                },
-        )
-        Column(
-            modifier = Modifier
-                .height(imageHeight)
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Bottom,
-        ) {
-            Text(
-                modifier = Modifier.fillMaxWidth(), text = "**** ${
-                    card.cardNumber.substring(
-                        card.cardNumber.length - 4, card.cardNumber.length
-                    )
-                }", color = Color.Red
+        Box {
+            Image(
+                painter = painterResource(id = card.cardImage),
+                contentDescription = "cardImage",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxWidth(),
             )
-
+            Column(
+                modifier = Modifier
+                    .matchParentSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Bottom,
+            ) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(), text = "**** ${
+                        card.cardNumber.substring(
+                            card.cardNumber.length - 4, card.cardNumber.length
+                        )
+                    }", color = Color.Red
+                )
+            }
         }
     }
-    return imageHeight
 }
 
 
 @Composable
-fun EmptyCardItem(height: Dp) {
+fun EmptyCardItem() {
     Card(
-        shape = RoundedCornerShape(10.dp),
-        backgroundColor = Color.LightGray,
+        shape = RoundedCornerShape(14.dp), backgroundColor = Color.LightGray.copy(alpha = 0.8f)
     ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(height)
-                .padding(16.dp),
+                .aspectRatio(111 / 69f),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -113,6 +152,98 @@ fun EmptyCardItem(height: Dp) {
                     .padding(horizontal = 25.dp, vertical = 8.dp))
         }
     }
+}
+
+
+@Composable
+fun QuickActionsUI() {
+    Column(
+        modifier = Modifier
+    ) {
+        Text(
+            text = "Quick Actions",
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp,
+            modifier = Modifier.padding(vertical = 10.dp)
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            QuickActionIcons(
+                iconId = R.drawable.ic_transfer,
+                contentDescription = "Send",
+            )
+            QuickActionIcons(
+                iconId = R.drawable.ic_exchange_op,
+                contentDescription = "Exchange",
+            )
+            QuickActionIcons(
+                iconId = R.drawable.ic_remittance,
+                contentDescription = "Remittance",
+            )
+            QuickActionIcons(
+                iconId = R.drawable.ic_transfer,
+                contentDescription = "Explore",
+            )
+        }
+    }
+}
+
+@Composable
+fun QuickActionIcons(
+    @DrawableRes iconId: Int, contentDescription: String, onClickListener: (() -> Unit)? = null
+) {
+    val iconModifier = Modifier
+        .size(41.60.dp)
+        .clip(CircleShape)
+        .clickable {
+            onClickListener?.invoke()
+        }
+        .background(color = Color.Magenta)
+        .padding(7.80.dp)
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Icon(
+            painterResource(id = iconId),
+            contentDescription = contentDescription,
+            modifier = iconModifier,
+            tint = Color.Unspecified
+        )
+        Text(
+            modifier = Modifier,
+            text = contentDescription,
+            textAlign = TextAlign.Center,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
+
+@Composable
+fun TransactionsUI(transactionList: ArrayList<TransactionItem>) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 10.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = "Latest transactions", fontWeight = FontWeight.Bold)
+            Text(text = "Show all", color = Color.Blue)
+        }
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            contentPadding = PaddingValues(vertical = 10.dp)
+        ) {
+            items(items = transactionList) {
+                TransactionItemUI(transaction = it)
+                Divider()
+            }
+        }
+    }
+
 }
 
 @Composable
@@ -131,79 +262,15 @@ fun TransactionItemUI(transaction: TransactionItem) {
 }
 
 @Composable
-fun QuickActionsUI() {
+@Preview(showBackground = true)
+fun CardItemPreview() {
+    val userCard = UserCard(1234, "23423", 321, "", "ranjan", "", "", "", "")
+    val transactionItem = TransactionItem(12, "USD", "Debit", "Remittance", "ranjan", "Now", "Done")
     Column {
-        Text(
-            text = "Quick Actions",
-            fontWeight = FontWeight.Bold,
-            fontSize = 16.sp,
-            modifier = Modifier.padding(10.dp)
-        )
-        Row(
-            modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            QuickActionIcons(
-                icon = Icons.Rounded.Send,
-                contentDescription = "Send",
-            )
-            QuickActionIcons(
-                icon = Icons.Default.AccountBox,
-                contentDescription = "Exchange",
-            )
-            QuickActionIcons(
-                icon = Icons.Rounded.AccountCircle,
-                contentDescription = "Remittance",
-            )
-            QuickActionIcons(
-                icon = Icons.Default.ExitToApp,
-                contentDescription = "Explore",
-            )
-        }
+//        cardItem(card = userCard)
+//        TransactionItemUI(transactionItem)
+//        QuickActionsUI()
+//        ActionIcons(Icons.Outlined.AccountBox, contentDescription = "hello")
     }
 }
 
-@Composable
-fun QuickActionIcons(
-    icon: ImageVector, contentDescription: String, onClickListener: (() -> Unit)? = null
-) {
-    val iconModifier = Modifier
-        .clip(CircleShape)
-        .clickable {
-            onClickListener?.invoke()
-        }
-        .background(color = Color.Yellow)
-        .padding(15.dp)
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Icon(
-            imageVector = icon, contentDescription = contentDescription, modifier = iconModifier
-        )
-        Text(
-            modifier = Modifier,
-            text = contentDescription,
-            textAlign = TextAlign.Center,
-            fontSize = 14.sp,
-            fontWeight = FontWeight.Bold,
-        )
-    }
-}
-
-
-@Composable
-fun ActionIcons(
-    icon: ImageVector,
-    contentDescription: String = "",
-    padding: Dp = 5.dp,
-    onClickListener: (() -> Unit)? = null
-) {
-    val iconModifier = Modifier
-        .clip(CircleShape)
-        .clickable {
-            onClickListener?.invoke()
-        }
-    Icon(
-        imageVector = icon,
-        contentDescription = contentDescription,
-        modifier = iconModifier.padding(padding)
-    )
-
-}
